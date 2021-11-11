ad5d4672b6377bf0871b83c236583f151cc402b5
Spencer Cai <shouspencercjh@foxmail.com>
2021-11-11 15:15:26 +0800

    upgrade to 1.1.5

--- Formula/sshctx.rb
+++ Formula/sshctx.rb
@@ -1,8 +1,8 @@
 class Sshctx < Formula
   desc "Faster way to switch between hosts in ssh"
   homepage "https://github.com/spencercjh/sshctx"
-  url "https://github.com/spencercjh/sshctx/releases/download/v1.1.0/sshctx_v1.1.0_darwin_x86_64.tar.gz"
-  sha256 "68ea7b76e3b780d2508630d6976b5b3a444ea292993ce2493c01a8fb5db8538d"
+  url "https://github.com/spencercjh/sshctx/releases/download/v1.1.5/sshctx_v1.1.5_darwin_x86_64.tar.gz"
+  sha256 "11fc66e60cd82b7dc5cef79aad060a26628f4c74abec00d6d1cfb64319d875a4"
   license "Apache-2.0"
 
   def install
